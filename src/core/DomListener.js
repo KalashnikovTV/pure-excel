8bb58c17df6353a3bdc8b72ed1925c387e64ad2e
KalashnikovTV <max.malaschkov@mail.ru>
2021-01-16 21:07:07 +0300

    Finish base table logic

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,4 +1,4 @@
-import {capitalize} from "./utilis";
+import {capitalize} from "./utils";
 
 export class DomListener {
     constructor($root, listeners = []) {
@@ -10,7 +10,6 @@ export class DomListener {
     }
 
     initDOMListeners() {
-        // console.log(this.listeners);
         this.listeners.forEach(listener => {
             const method = getMethodName(listener);
             if (!this[method]) {
@@ -18,7 +17,6 @@ export class DomListener {
                 throw new Error(
                     `Method ${method} is not implemented in ${name} Component`);
             }
-            // Тоже самое что и addEventListener
             this[method] = this[method].bind(this);
             this.$root.on(listener, this[method]);
         });
@@ -32,7 +30,6 @@ export class DomListener {
     }
 }
 
-// input => onInput
 function getMethodName(eventName) {
     return 'on' + capitalize(eventName);
 }
